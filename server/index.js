0de96b62010a446eb64f83455f97224915642332
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-03-13 12:16:29 +0530

    DB connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
@@ -15,7 +16,13 @@ app.get("/", (req, res) => {
 const StartServer = async () => {
   try {
     //connect to data base
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("server started 🚀.. on port http://localhost:8080")
+    );
   } catch (err) {
     console.log(err);
   }
 };
+
+StartServer();
--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB=(url)={
-    mongoose.set('stricyQuery',true)
-}
+const connectDB = (url) => {
+  mongoose.set("strictQuery", true);
+
+  mongoose
+    .connect(url)
+    .then(() => console.log("Connected to DB 🪖..."))
+    .catch((error) => console.log(console.log(error)));
+};
+
+export default connectDB;
