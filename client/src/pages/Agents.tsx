a2763c162c946b737a57f9c547ea6a38b16bb94f
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-03-18 13:09:10 +0530

    profiles,agents added

--- client/src/pages/Agents.tsx
+++ client/src/pages/Agents.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useList } from "@pankod/refine-core";
+import { Box, Typography } from "@mui/material";
+
+import { AgentCard } from "components";
 
 const Agents = () => {
-  return <div>Agents</div>;
+  const { data, isLoading, isError } = useList({ resource: "users" });
+
+  const allAgents = data?.data ?? [];
+  console.log("all agents : ", allAgents);
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>error...</div>;
+
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Agents List
+      </Typography>
+
+      <Box
+        mt="20px"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          backgroundColor: "#fcfcfc",
+        }}
+      >
+        {allAgents.map((agent) => (
+          <AgentCard
+            key={agent._id}
+            id={agent._id}
+            name={agent.name}
+            email={agent.email}
+            avatar={agent.avatar}
+            noOfProperties={agent.allProperties.length}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
 };
 
 export default Agents;
--- client/src/pages/MyProfile.tsx
+++ client/src/pages/MyProfile.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
+import { useGetIdentity, useOne } from "@pankod/refine-core";
+
+import { Profile } from "components";
+
 const MyProfile = () => {
-  return <div>My profile</div>;
+  const { data: user } = useGetIdentity();
+  console.log("user :", user);
+
+  const { data, isLoading, isError } = useOne({
+    resource: "users",
+    id: user?.user_id,
+  });
+  console.log("data : ", data);
+
+  const myProfile = data?.data ?? [];
+
+  console.log("myProfile  : ", myProfile);
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>error...</div>;
+
+  return (
+    <Profile
+      type="My"
+      name={myProfile.name}
+      email={myProfile.email}
+      avatar={myProfile.avatar}
+      properties={myProfile.allProperties}
+    />
+  );
 };
 
 export default MyProfile;
