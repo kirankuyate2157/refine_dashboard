e8980fdeaeb49a4b7aaca9ddd8f35555cf94b995
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-04-11 00:49:39 +0530

    mail send  api addded

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" })); //setting limit
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hello World my name is kiran.dev" }); //route response on root
+  res.send({ message: "Hello World ! my name is kiran.dev" }); //route response on root
 });
 //using middleware  "api/v1/users"
 
